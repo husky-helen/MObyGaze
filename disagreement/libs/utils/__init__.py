d9588472c92b3704237a29aee955c0b3fdf38f98
trsjulie <37087267+trsjulie@users.noreply.github.com>
2024-06-06 15:45:26 +0200

    Delete disagreement directory

--- disagreement/libs/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-
-from .train_utils import ( save_checkpoint,
-                          AverageMeter, train_one_epoch, valid_one_epoch, valid_one_epoch_bms,
-                          fix_random_seed)
-
-from .metric_utils import BMS
-__all__ = [ 'save_checkpoint', 'AverageMeter', 'train_one_epoch', 'valid_one_epoch', 'ANETdetection',
-           'fix_random_seed']
